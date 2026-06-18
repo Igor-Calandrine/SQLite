38d12358f1650f1f17261fda1190cc7bf7511b20
Igor Calandrine <igorwow@hotmail.com>
2026-06-18 17:56:07 -0300

    update

---
+++ 6-Update/queries.sql
@@ -0,0 +1,22 @@
+CREATE TABLE produtos (
+   "id" INTEGER NOT NULL,
+   "nome" TEXT NOT NULL,
+   "quantidade" INTEGER NOT NULL,
+   "preço" REAL NOT NULL
+);
+
+INSERT INTO produtos ("id", "nome", "quantidade", "preço") VALUES
+(1, 'banana', 10, 5.50),
+(2, 'pera', 5, 8.50),
+(3, 'uva', 8, 3.50),
+(4, 'maça', 3, 2.50),
+(5, 'abacaxi', 15, 9),
+(6, 'couve', 20, 10);
+
+UPDATE produtos SET "preço" = 2 WHERE "nome" = 'couve';
+UPDATE produtos SET "quantidade" = 35 WHERE "id" = 5;
+
+SELECT * FROM produtos;
+SELECT "nome", "preço" FROM produtos;
+
+
